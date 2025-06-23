0dcb394b069a5b23ddd46930234397304a56b360
shlbruce <shlbruce@hotmail.com>
2025-06-23 14:39:31 -0500

    add analyze button on replies

--- content.js
+++ content.js
@@ -19,6 +19,24 @@ function addButtonsToAnalyze() {
   commentSectionList.forEach((commentSection) => {
     addAnalyzeButton(commentSection);
   });
+
+  const repliesSectionList = commentsSection.querySelectorAll('div[id="replies"]');
+  repliesSectionList.forEach((replySection) => {
+    
+    const contentsSecton = replySection.querySelector('div[id="contents"]');
+    if (!contentsSecton) {
+      return;
+    }
+    contentSectionList = contentsSecton.querySelectorAll('ytd-comment-view-model')
+    if (contentSectionList.length === 0) {
+      return;
+    }
+    contentsSecton.id = "contents-1";
+    contentSectionList.forEach((commentSection) => {
+      // Add analyze button to each reply comment
+      addAnalyzeButton(commentSection);
+    });
+  });
 }
 
 // Run initially and observe DOM changes
