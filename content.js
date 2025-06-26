0b3b13ea923eba03ad49dd5918575a9d89bc5c04
shlbruce <shlbruce@hotmail.com>
2025-06-26 10:43:28 -0500

    re-org codes to support multiple websites

--- content.js
+++ content.js
@@ -1,43 +1,10 @@
-
-
-function setupButtonsToAnalyze() {
-
-  if (!document.getElementById('analyze-all-button')) {
-    setupAnalyzeAllButton();
-    //sortNewestFirst();
-  }
-
-  const commentsSection = document.getElementById('comments');
-  if (!commentsSection) {
-    return;
-  }
-  
-  const commentSectionList = commentsSection.querySelectorAll('ytd-comment-view-model[id="comment"]');
-
-  commentSectionList.forEach((commentSection) => {
-    setupAnalyzeButton(commentSection);
-  });
-
-  const repliesSectionList = commentsSection.querySelectorAll('div[id="replies"]');
-  repliesSectionList.forEach((replySection) => {
-    
-    const contentsSecton = replySection.querySelector('div[id="contents"]');
-    if (!contentsSecton) {
+function init() {
+  const hostname = window.location.hostname;
+  if (hostname === "www.youtube.com") {
+      setupAnalyzeButtonsInYoutube();
       return;
-    }
-    contentSectionList = contentsSecton.querySelectorAll('ytd-comment-view-model')
-    if (contentSectionList.length === 0) {
-      return;
-    }
-    contentsSecton.id = "contents-1";
-    contentSectionList.forEach((commentSection) => {
-      // Add analyze button to each reply comment
-      setupAnalyzeButton(commentSection);
-    });
-  });
+  }
 }
 
-// Run initially and observe DOM changes
-setupButtonsToAnalyze();
-const observer = new MutationObserver(setupButtonsToAnalyze);
+const observer = new MutationObserver(init);
 observer.observe(document.body, { childList: true, subtree: true });  
