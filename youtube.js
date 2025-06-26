4c0f5a687640334e40b70e3f22dfe4edddcfce99
shlbruce <shlbruce@hotmail.com>
2025-06-26 10:47:16 -0500

    re-org

--- youtube.js
+++ youtube.js
@@ -1,36 +1,40 @@
 function setupAnalyzeButtonsInYoutube() {
 
     if (!document.getElementById('analyze-all-button')) {
-      setupAnalyzeAllButton();
-      //sortNewestFirst();
+        setupAnalyzeAllButton();
+        //sortNewestFirst();
     }
-  
+
+    setupAllAnalyzeButton();
+}
+
+function setupAllAnalyzeButton() {
     const commentsSection = document.getElementById('comments');
     if (!commentsSection) {
-      return;
+        return;
     }
-    
+
     const commentSectionList = commentsSection.querySelectorAll('ytd-comment-view-model[id="comment"]');
-  
+
     commentSectionList.forEach((commentSection) => {
-      setupAnalyzeButton(commentSection);
+        setupAnalyzeButton(commentSection);
     });
-  
+
     const repliesSectionList = commentsSection.querySelectorAll('div[id="replies"]');
     repliesSectionList.forEach((replySection) => {
-      
-      const contentsSecton = replySection.querySelector('div[id="contents"]');
-      if (!contentsSecton) {
-        return;
-      }
-      contentSectionList = contentsSecton.querySelectorAll('ytd-comment-view-model')
-      if (contentSectionList.length === 0) {
-        return;
-      }
-      contentsSecton.id = "contents-1";
-      contentSectionList.forEach((commentSection) => {
-        // Add analyze button to each reply comment
-        setupAnalyzeButton(commentSection);
-      });
+
+        const contentsSecton = replySection.querySelector('div[id="contents"]');
+        if (!contentsSecton) {
+            return;
+        }
+        contentSectionList = contentsSecton.querySelectorAll('ytd-comment-view-model')
+        if (contentSectionList.length === 0) {
+            return;
+        }
+        contentsSecton.id = "contents-1";
+        contentSectionList.forEach((commentSection) => {
+            // Add analyze button to each reply comment
+            setupAnalyzeButton(commentSection);
+        });
     });
-  }
+}
